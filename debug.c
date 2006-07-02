f73b4e709c28f28e53db737f064acceafa776979
Radek Podgorny <radek@podgorny.cz>
2006-07-02 18:19:59 +0200

    Forgot to add debug.c file

---
+++ debug.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+
+FILE* dbgfile = NULL;
+
+int debug_init() {
+#ifdef DEBUG
+	char *dbgpath = "./unionfs_debug.log";
+	printf("Debug mode, log will be written to %s\n", dbgpath);
+
+	dbgfile = fopen(dbgpath, "w");
+	if (!dbgfile) {
+		printf("Failed to open %s for writing, exitting\n", dbgpath);
+		return 2;
+	}
+#endif
+	return 0;
+}
