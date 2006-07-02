1b0829a4ad508b53b370f455d9dee1b252cf7d36
Radek Podgorny <radek@podgorny.cz>
2006-07-02 18:13:32 +0200

    Little stats refactoring

--- stats.c
+++ stats.c
@@ -4,6 +4,10 @@
 #include "stats.h"
 
 
+unsigned int stats_read_b, stats_read_k, stats_read_m, stats_read_g, stats_read_t;
+unsigned int stats_written_b, stats_written_k, stats_written_m, stats_written_g, stats_written_t;
+
+
 void stats_init() {
 	stats_enabled = 0;
 
