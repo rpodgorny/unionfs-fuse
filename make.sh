0a8ff1ee4e628aa28533250247aec6c2cb07f941
Radek Podgorny <radek@podgorny.cz>
2006-08-04 22:04:28 +0200

    Improved make script

--- make.sh
+++ make.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-CFLAGS="${CFLAGS:--Wall}"
-CPPFLAGS="$CPPFLAGS -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26"
-LDFLAGS="$LDFLAGS -lfuse"
+[ -z ${CC} ] && CC=gcc
 
-gcc $CPPFLAGS $CFLAGS $LDFLAGS -o unionfs *.c "$@"
+CFLAGS="${CFLAGS:--Wall}"
+CPPFLAGS="${CPPFLAGS} -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26"
+LDFLAGS="${LDFLAGS} -lfuse"
+
+${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o unionfs *.c "$@"
