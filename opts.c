2d9c62f97c8326c974a119ffc3c9128a2cb33622
Radek Podgorny <radek@podgorny.cz>
2007-02-10 00:36:14 +0100

    Added a warning

--- opts.c
+++ opts.c
@@ -62,7 +62,10 @@ void add_root(char *root) {
 	if (res) {
 		if (strcasecmp(res, "rw") == 0) {
 			uopt.roots[uopt.nroots].rw = 1;
+		} else if (strcasecmp(res, "ro") == 0) {
+			uopt.roots[uopt.nroots].rw = 0;
 		} else {
+			fprintf(stderr, "Failed to parse RO/RW flag, setting RO.\n");
 			uopt.roots[uopt.nroots].rw = 0;
 		}
 	}
