9cef6a4fdc8df79df65ed492aef69ac0e2a545f5
Radek Podgorny <radek@podgorny.cz>
2007-02-11 17:39:26 +0100

    Added cache cleaning

--- cache.c
+++ cache.c
@@ -8,10 +8,35 @@
 #include "opts.h"
 #include "hash.h"
 #include "hashtable.h"
+#include "hashtable_itr.h"
 
 
 struct hashtable *cache;
 
+
+// Thow away the old records
+static void cache_clean_old() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	struct hashtable_itr *itr = hashtable_iterator(cache);
+	do {
+		// Maybe a bug in hastable library? We solve it this way
+		if (!itr->e) break;
+
+		cache_entry_t *e = hashtable_iterator_value(itr);
+
+		if ((tv.tv_sec - e->time) > uopt.cache_time) {
+			// too old
+			hashtable_iterator_remove(itr);
+			free(e);
+
+			// continue since the iterator_remove advances the iterator
+			continue;
+		}
+	} while (hashtable_iterator_advance(itr) != 0);
+	free(itr);
+}
 
 void cache_init() {
 	cache = create_hashtable(16, string_hash, string_equal);
@@ -20,6 +45,12 @@ void cache_init() {
 int cache_lookup(const char *path) {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
+
+	static time_t last_clean_time = 0; // when we last threw away old entries
+	if ((tv.tv_sec - last_clean_time) > uopt.cache_time) {
+		// it's time for the cleanup
+		cache_clean_old();
+	}
 
 	cache_entry_t *e = hashtable_search(cache, (void *)path);
 
