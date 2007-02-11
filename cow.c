6cfe8ce7051eb4e9c1a7c5388a34f2eccb9b2a8f
Radek Podgorny <radek@podgorny.cz>
2007-02-12 00:53:36 +0100

    Haha! More bytes saved!

--- cow.c
+++ cow.c
@@ -16,27 +16,25 @@
  * initiate the cow-copy action
  */
 static int cow_cp(const char *path, int root_ro, int root_rw) {
-	struct cow cow;
 	char from[PATHLEN_MAX], to[PATHLEN_MAX];
-
 	snprintf(from, PATHLEN_MAX, "%s%s", uopt.roots[root_ro].path, path);
 	snprintf(to, PATHLEN_MAX, "%s%s", uopt.roots[root_rw].path, path);
 
-	(void)setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
+
+	struct cow cow;
 
 	cow.uid = getuid();
 
 	// Copy the umask for explicit mode setting.
 	cow.umask = umask(0);
-	(void)umask(cow.umask);
+	umask(cow.umask);
 
 	cow.from_path = from;
-	cow.to_path   = to;
+	cow.to_path = to;
 
 	struct stat buf;
-	int res;
-
-	res = lstat(cow.from_path, &buf);
+	int res = lstat(cow.from_path, &buf);
 	cow.stat = &buf;
 
 	switch (buf.st_mode & S_IFMT) {
@@ -50,8 +48,7 @@ static int cow_cp(const char *path, int root_ro, int root_rw) {
 		case S_IFIFO:
 			return copy_fifo(&cow);
 		case S_IFSOCK:
-			syslog(LOG_WARNING, "COW of sockets not supported: %s\n",
-			       cow.from_path);
+			syslog(LOG_WARNING, "COW of sockets not supported: %s\n", cow.from_path);
 			return 1;
 		default:
 			return copy_file(&cow);
