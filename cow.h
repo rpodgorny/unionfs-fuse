528971ab842d8aae918845c7364cc24ccbaa1220
Radek Podgorny <radek@podgorny.cz>
2007-02-12 02:23:57 +0100

    Moved functions around...

--- cow.h
+++ cow.h
@@ -3,6 +3,9 @@
 
 #include <sys/stat.h>
 
+#define VM_AND_BUFFER_CACHE_SYNCHRONIZED
+#define MAXBSIZE 4096
+
 struct cow {
 	mode_t umask;
 	uid_t uid;
@@ -15,13 +18,8 @@ struct cow {
 	char *to_path;
 };
 
-#define VM_AND_BUFFER_CACHE_SYNCHRONIZED
-#define MAXBSIZE 4096
-
+int cow_cp(const char *path, int root_ro, int root_rw);
 int path_create(const char *path, int nroot_ro, int nroot_rw);
 int path_create_cutlast(const char *path, int nroot_ro, int nroot_rw);
-
-int cow_cp(const char *path, int root_ro, int root_rw);
-
 
 #endif
