85fdc894cbb46df6cc58a9a04f0d0f1d02460987
Bernd Schubert <bernd-schubert@gmx.de>
2008-04-20 16:56:58 +0200

    Proper copy-on-write support for rmdir().

--- rmdir.c
+++ rmdir.c
@@ -21,6 +21,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include <libgen.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #include "unionfs.h"
 #include "opts.h"
@@ -28,6 +30,113 @@
 #include "cow.h"
 #include "general.h"
 #include "findbranch.h"
+
+enum {
+	EMPTY = 0,
+	HAS_WHITEOUTS
+};
+
+
+/**
+  * path might have whiteout files which can't be deleted from
+  * outside of unionfs, test for these files
+  */
+static int test_for_whiteouts(const char *path) {
+	DIR *dp = opendir(path);
+	if (dp == NULL) return errno;
+	int res = EMPTY;
+
+	struct dirent *de;
+	while ((de = readdir(dp)) != NULL) {
+		if (strncmp (de->d_name, ".", 1) == 0
+		||  strncmp (de->d_name, "..", strlen(de->d_name)) == 0)
+			continue;
+
+		if (!whiteout_tag(de->d_name)) {
+			closedir (dp);
+			return ENOTEMPTY;
+		} else {
+			res = HAS_WHITEOUTS;
+			continue;
+		}
+	}
+	return res;
+}
+
+/**
+  * recursively delete a directory
+  */
+static int recursive_rmdir(const char *path)
+{
+	int res;
+	char fname[PATHLEN_MAX];
+
+	DIR *dp = opendir(path);
+	if (dp == NULL) return errno;
+
+	struct dirent *de;
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+
+		if (strncmp (de->d_name, ".", 1) == 0
+		||  strncmp (de->d_name, "..", strlen(de->d_name)) == 0)
+			continue;
+
+		if (string_too_long(2, path, de->d_name)) return ENAMETOOLONG;
+		sprintf (fname, "%s/%s", path, de->d_name);
+
+		if (lstat(fname, &st) == 0) {
+			if (S_ISDIR(st.st_mode)) {
+				res = recursive_rmdir(fname);
+				if (res) {
+					errno = res;
+					goto err_out;
+				}
+			} else {
+				// file, delete it
+				res = unlink(fname);
+				if (res == -1) goto err_out;
+			}
+		} else goto err_out;
+	}
+	res = rmdir (path);
+	if (res) goto err_out;
+
+	closedir (dp);
+	return 0;
+
+err_out:
+	closedir (dp);
+	return errno;
+}
+
+/**
+  * If the root that has the directory to be removed is in read-write mode,
+  * we can really delete the file.
+  */
+static int rmdir_rw(const char *path, int root_rw) {
+	char p[PATHLEN_MAX];
+	snprintf(p, PATHLEN_MAX, "%s%s", uopt.roots[root_rw].path, path);
+
+	int res = test_for_whiteouts(p);
+
+	if (res < 0)
+		return res; // directory probably not empty
+	else if (res == EMPTY) {
+		// directory is empty, we can delete it
+		res = rmdir(p);
+		if (res == -1) return errno;
+	} else {
+		// directory has whiteout files
+		res = recursive_rmdir(p);
+		if (res) return res;
+
+		// since there had been whiteout files within path, we really should hide lower branches
+		hide_dir (path, root_rw);
+	}
+
+	return 0;
+}
 
 /**
   * If the root that has the directory to be removed is in read-only mode,
@@ -44,28 +153,13 @@ static int rmdir_ro(const char *path, int root_ro) {
 	if (root_rw >= 0) i = path_create_cutlast(path, root_ro, root_rw);
 
 	// no writable path, or some other error
-	if (i < 0) return -EACCES;
+	if (i < 0) return EACCES;
 
 	if (hide_dir(path, root_rw) == -1) {
 		// creating the file with the hide tag failed
 		// TODO: open() error messages are not optimal on rmdir()
-		return -errno;
+		return errno;
 	}
-
-	return 0;
-}
-
-/**
-  * If the root that has the directory to be removed is in read-write mode,
-  * we can really delete the file.
-  */
-static int rmdir_rw(const char *path, int root_rw) {
-	char p[PATHLEN_MAX];
-	snprintf(p, PATHLEN_MAX, "%s%s", uopt.roots[root_rw].path, path);
-
-	int res = rmdir(p);
-
-	if (res == -1) return -errno;
 
 	return 0;
 }
@@ -89,12 +183,12 @@ int unionfs_rmdir(const char *path) {
 	if (!uopt.roots[i].rw && uopt.cow_enabled) {
 		res = rmdir_ro(path, i);
 		to_root();
-		return res;
+		return -res;
 	}
 	
 	res = rmdir_rw(path, i);
 	to_root();
 	if (res == 0)
 		unionfs_rmdir(path); /* make _real_ sure the directory is removed */
-	return res;
+	return -res;
 }
