2642d74006bf7bca356c83888249860c3101e026
Bernd Schubert <bernd.schubert@fastmail.fm>
2009-06-02 22:52:28 +0200

    Man page and example fixes.

--- examples/S01a-unionfs-fuse-live-cd.sh
+++ examples/S01a-unionfs-fuse-live-cd.sh
@@ -3,7 +3,7 @@
 # Copyright: Bernd Schubert <bernd.schubert@fastmail.fm>
 # BSD license, see LICENSE file for details
 
-FUSE_OPT="-o default_permissions,allow_other,use_ino,suid"
+FUSE_OPT="-o allow_other,use_ino,suid,dev,nonempty"
 CHROOT_PATH="/tmp/unionfs"
 UNION_OPT="-ocow,chroot=$CHROOT_PATH,max_files=32768"
 
@@ -20,7 +20,6 @@ mount --bind / $CHROOT_PATH/root
 
 $UBIN $FUSE_OPT $UNION_OPT /rw=RW:/root=RO /tmp/union
 
-mount --bind /dev /tmp/union/dev
 mount -t proc proc /tmp/union/proc
 
 cd /tmp/union
