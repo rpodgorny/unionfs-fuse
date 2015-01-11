9bdc12905b9924e1bce7fe3097f2fa67d392416f
Radek Podgorny <radek@podgorny.cz>
2015-01-11 12:14:42 +0100

    whitespace formatting

--- src/uioctl.h
+++ src/uioctl.h
@@ -1,7 +1,7 @@
 /*
 * License: BSD-style license
 * Copyright: Bernd Schubert <bernd.schubert@fastmail.fm>
-*            
+* 
 */
 
 #ifndef UIOCTL_H_
@@ -13,10 +13,11 @@
 
 
 enum unionfs_ioctls {
-	UNIONFS_ONOFF_DEBUG    = _IOW('E', 0, int),
-	UNIONFS_SET_DEBUG_FILE = _IOW('E', 1, char[PATHLEN_MAX]),
+	UNIONFS_ONOFF_DEBUG         = _IOW('E', 0, int),
+	UNIONFS_SET_DEBUG_FILE      = _IOW('E', 1, char[PATHLEN_MAX]),
+	UNIONFS_STATS_BYTES_READ    = _IOW('E', 2, void),
+	UNIONFS_STATS_BYTES_WRITTEN = _IOW('E', 3, void),
 } unionfs_ioctls_t;
 
 #endif // UIOCTL_H_
-
 
