3b056a2794e23a91d6069fafd7b656172d09f8e7
Magnus Ottenklinger <magnus.ottenklinger@entelios.com>
2015-06-11 16:40:02 +0200

    use proper temporary file

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 import shutil
 import time
+import tempfile
 
 
 def call(cmd):
@@ -236,18 +237,14 @@ class IOCTL_TestCase(Common, unittest.TestCase):
 	def setUp(self):
 		super().setUp()
 		call('src/unionfs rw1=rw:ro1=ro union')
+		self._temp_dir = tempfile.mkdtemp()
 	#enddef
 
 	def test_debug(self):
-		# TODO: this is not safe, use some temporary filename or something
-		if os.path.exists('/tmp/test_debug.log'):
-			os.remove('/tmp/test_debug.log')
-		#endif
-
-		call('src/unionfsctl -p /tmp/test_debug.log -d on union')
-
-		self.assertTrue(os.path.isfile('/tmp/test_debug.log'))
-		os.remove('/tmp/test_debug.log')
+		temp_file = os.path.join(self._temp_dir, 'debug.log')
+		call("src/unionfsctl -p %r -d on union" %temp_file)
+		self.assertTrue(os.path.isfile(temp_file))
+		self.assertEqual(read_from_file(temp_file), '')
 	#enddef
 
 	def test_wrong_args(self):
@@ -257,6 +254,10 @@ class IOCTL_TestCase(Common, unittest.TestCase):
 		ex = contextmanager.exception
 		self.assertEqual(ex.returncode, 1)
 		self.assertEqual(ex.output, b'')
+	#enddef
+	def tearDown(self):
+		super().tearDown()
+		shutil.rmtree(self._temp_dir)
 	#enddef
 #endclass
 
