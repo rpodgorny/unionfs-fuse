rpodgorny/unionfs-fuse

commit dbf2affe49b846c4438a43ee6357f8b689299c1c

Add partial support for travis
This fixes issues with fusermount -u in usermodelinux. There, we have to use umount for
unmounting. It also skips ioctl tests, as ubuntu precise on travis struggles with the
debug ioctl and fails that test with ENOTTY (inappropriate ioctl).

test.py
@@ -52,7 +52,13 @@ class Common:
 		# if it fails for someone, let's find the race and fix it!
 		#time.sleep(1)
 
-		call('fusermount -u union')
+		# When running the testsuite within usermodelinux, /dev/mtab might not
+		# exist. In that case, fusermount -u will fail. We thus fall back to
+		# umount.
+		if os.path.isfile('/dev/mtab'):
+			call('fusermount -u union')
+		else:
+			call('umount union')
 
 		for d in self._dirs:
 			shutil.rmtree(d)
@@ -325,6 +331,7 @@ class UnionFS_RO_RW_COW_TestCase(Common, unittest.TestCase):
 #endclass
 
 
+@unittest.skipIf(not os.environ.get('RUNNING_ON_TRAVIS_CI'), 'Not supported on Travis')
 class IOCTL_TestCase(Common, unittest.TestCase):
 	def setUp(self):
 		super().setUp()

umltest.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-CURDIR="`pwd`"
+CURDIR="$(pwd)"
+VIRTUALENV_PATH="$(realpath ~/virtualenv)"
+echo "VIRTUALENV_PATH: ${VIRTUALENV_PATH}"
 
 cat > umltest.inner.sh <<EOF
 #!/bin/sh
 (
 	set -e
 	set -x
+        # source python3.5, test.py depends on python >= 3.3 (PermissionError)
+	. "${VIRTUALENV_PATH}"/python3.5/bin/activate
 	insmod /usr/lib/uml/modules/\`uname -r\`/kernel/fs/fuse/fuse.ko
 	cd "$CURDIR"
-	python3 test.py
+	python3 --version
+        # sleep if it fails to allow writing stuff
+	RUNNING_ON_TRAVIS_CI= python3 test.py
 	echo Success
 )
 echo "\$?" > "$CURDIR"/umltest.status
@@ -18,6 +24,8 @@ EOF
 
 chmod +x umltest.inner.sh
 
-/usr/bin/linux.uml init=`pwd`/umltest.inner.sh rootfstype=hostfs rw
+/usr/bin/linux.uml init="${CURDIR}"/umltest.inner.sh rootfstype=hostfs rw
 
-exit $(<umltest.status)
+RESULT=$(<"${CURDIR}"/umltest.status)
+
+exit $RESULT

.travis.yml
@@ -1,11 +1,13 @@
-# TODO: this is broken as travis does not support fuse
-
-language: c
+language: python
+python:
+  - "3.5"
 before_install:
   - sudo apt-get update -qq
 install:
+  - sudo apt-get install realpath
+  - sudo apt-get install cmake make gcc
   - sudo apt-get install -qq fuse fuse-utils libfuse-dev
-  - sudo apt-get install -qq python3 user-mode-linux
+  - sudo apt-get install -qq user-mode-linux
   - sudo mknod /dev/fuse c 10 229
   - sudo chmod 666 /dev/fuse
   - cmake .

commit f0495a954cdf20794f578726333b5addbff0fc4b

Fixup path to /etc/mtab
mtab is placed in /etc. The regression in dbf2affe49b broke the
testsuite on systems where no file /dev/mtab exists and the user
executing the test did not have sufficient rights to call umount.

test.py
@@ -7,7 +7,6 @@ import shutil
 import time
 import tempfile
 
-
 def call(cmd):
 	return subprocess.check_output(cmd, shell=True)
 #enddef
@@ -53,13 +52,13 @@ class Common:
 		# actually had to re-enable it because travis-ci is one of the bad cases
 		time.sleep(1)
 
-		# When running the testsuite within usermodelinux, /dev/mtab might not
-		# exist. In that case, fusermount -u will fail. We thus fall back to
-		# umount.
-		if os.path.isfile('/dev/mtab'):
-			call('fusermount -u union')
-		else:
+		# In User Mode Linux, fusermount -u union fails with a permission error
+		# when trying to lock the fuse lock file.
+
+		if os.environ.get('RUNNING_ON_TRAVIS_CI'):
 			call('umount union')
+		else:
+			call('fusermount -u union')
 
 		for d in self._dirs:
 			shutil.rmtree(d)
@@ -332,7 +331,7 @@ class UnionFS_RO_RW_COW_TestCase(Common, unittest.TestCase):
 #endclass
 
 
-@unittest.skipIf(not os.environ.get('RUNNING_ON_TRAVIS_CI'), 'Not supported on Travis')
+@unittest.skipIf(os.environ.get('RUNNING_ON_TRAVIS_CI'), 'Not supported on Travis')
 class IOCTL_TestCase(Common, unittest.TestCase):
 	def setUp(self):
 		super().setUp()

umltest.sh
@@ -21,7 +21,7 @@ rm -r ro1 ro2 rw1 rw2 union
 	cd "$CURDIR"
 	python3 --version
         # sleep if it fails to allow writing stuff
-	RUNNING_ON_TRAVIS_CI= python3 test.py
+	RUNNING_ON_TRAVIS_CI=1 python3 test.py
 	echo Success
 )
 echo "\$?" > "$CURDIR"/umltest.status