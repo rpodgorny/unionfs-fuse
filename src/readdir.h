fd6102331bf68528f56ce9a877eb57a7b5a8191d
Radek Podgorny <radek@podgorny.cz>
2016-12-13 20:49:38 +0100

    initial fuse3 changes

--- src/readdir.h
+++ src/readdir.h
@@ -3,7 +3,7 @@
 
 #include <fuse.h>
 
-int unionfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+int unionfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t off, struct fuse_file_info *fi, enum fuse_readdir_flags flags);
 int dir_not_empty(const char *path);
 
 #endif
